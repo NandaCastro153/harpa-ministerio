dda2c9800e0a5d852a72fd79c266981aa659cb9c
NandaCastro153 <nandacriscastro1@hotmail.com>
2026-07-13 16:41:56 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Harpa PWA — Service Worker v16
-const CACHE_VERSION = 'harpa-v16';
+// Harpa PWA — Service Worker v17
+const CACHE_VERSION = 'harpa-v17';
 const ASSETS = ['/', '/index.html', '/manifest.json',
   'https://unpkg.com/react@18/umd/react.production.min.js',
   'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
